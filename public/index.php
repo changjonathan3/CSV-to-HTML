67bd268494ccfea2e9671e76aae74c6f637f1700
chang <jkc26@njit.edu>
2019-02-06 09:07:36 -0500

    Docs: Added an example csv table to test

--- public/index.php
+++ public/index.php
@@ -5,5 +5,47 @@
  * Date: 2/5/2019
  * Time: 10:24 PM
  */
+main::start();
+class main {
+    static public function start($fileName){
+        $records = csv::getRecords($fileName);
+        print_r($records);
+    }
+}
 
-echo 'test';
+class csv{
+    static public function getRecords($fileName){
+        $file = fopen($fileName, "r");
+        while(! feof($file)){
+            $record = fgetcsv($file);
+            $records [] = recordFactory::create($record);
+        }
+        fclose($file);
+        return $records;
+
+    }
+}
+
+class html{
+
+}
+
+class system{
+
+}
+
+class record
+{
+    public function __construct(Array $record = null){
+        print_r($record);
+}
+}
+
+class recordFactory
+{
+    public static function create(Array $array = null)
+    {
+        $record = new record($array);
+        return $record;
+    }
+}
