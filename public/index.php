5b845f3a96b6292ab9a631a997af2b629beda2ea
chang <jkc26@njit.edu>
2019-02-07 11:28:10 -0500

    Refactor: Added commentary

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: chang
+ * @author Jonathan Chang
  * Date: 2/5/2019
  * Time: 10:24 PM
+ * For IS 601 Project 1
  */
 
 main::start('example.csv');
@@ -17,60 +18,72 @@ class main {
 }
 
 class system{
+    /**
+     * @param $table Return the printed table
+     */
     public static function printTable($table){
         echo $table;
     }
 }
 
 class html{
+    /**
+     * @param $allRecords
+     *          Take the array generated from the CSV file and output it to an HTML table
+     */
     public static function makeTable($allRecords){
         $count = 0;
         // start table
         echo "<html lang=\"en\">
-<head>
+  <head>
     <!-- Required meta tags -->
     <meta charset=\\'utf - 8\\>
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
     <!-- Bootstrap CSS -->
     <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
     <title>Jonathan Chang</title>
-</head>
-<table class=\"table table-striped\">
+  </head>
+  <table class=\"table table-striped\">
     <thead>";
 
         foreach($allRecords as $record){
             if ($count == 0){
                 $array = $record -> returnArray();
                 $fields = array_keys($array);
+                //output the header rows
                 self::dataOut($fields, $style = 1);
 
                 echo "         
     </thead>
          <tbody>";
 
+                //output the first data row
                 $values = array_values($array);
                 self::dataOut($values);
             }
             else{
                 $array = $record -> returnArray();
                 $values = array_values($array);
-                //$html .= '<tr>';
-                //print_r($values);
+                //output the other data rows
                 self::dataOut($values);
             }
             $count++;
-            //print_r($record);
         }
         echo
         "</tbody>
-    </table>
+  </table>
 </html>";
         return ;
     }
 
+    /**
+     * @param array|null $array
+     *        Read in either the header row array or the data row array
+     * @param int $style
+     *        Read in either 1 for header row markup or 2 for data row markup
+     */
     private static function dataOut(Array $array = null, $style = 2){
         echo '<tr>';
-
         foreach($array as $data){
             if($style ==1){
                 echo '<th>' . htmlspecialchars($data) . '</th>';
@@ -84,6 +97,10 @@ class html{
 }
 
 class csv{
+    /**
+     * @param $file The CSV input file to read
+     * @return array The output array to be transformed
+     */
     public static function getRecords($file){
         $file = fopen($file,"r");
         $header = array();
@@ -106,22 +123,48 @@ class csv{
 }
 
 class record{
+    /**
+     * record constructor.
+     * @param array|null $header
+     *          Header row in file
+     * @param null $values
+     *          Data values mapped to column headers
+     */
     public function __construct(Array $header = null, $values = null){
         $c=array_combine($header,$values);
         foreach($c as $key => $value){
             $this ->createProperty($key, $value);
         }
     }
+
+    /**
+     * @return array CSV file read into array
+     */
     public function returnArray(){
         $array = (array) $this;
         return $array;
     }
+
+    /**
+     * @param string $name
+     *      Header column values
+     * @param string $value
+     *      Data cell values
+     */
     private function createProperty($name = 'first', $value = 'Adam'){
         $this ->{$name} = $value;
     }
 }
 
 class recordFactory{
+    /**
+     * @param array|null $header
+     *          Header column values
+     * @param null $values
+     *          Data cell values
+     * @return record
+     *          Header and Data pair
+     */
     public static function create(Array $header = null, $values = null){
 
         $record = new record($header, $values);
