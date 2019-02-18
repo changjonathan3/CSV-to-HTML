214aef2ee45ffbc0bbade99353359fcfc48f17fa
chang <jkc26@njit.edu>
2019-02-18 12:54:16 -0500

    Task: Refactor html to separate functions

--- public/index.php
+++ public/index.php
@@ -13,84 +13,72 @@ class main {
     public static function start($file){
         $allRecords = csv::getRecords($file);
         $table = html::makeTable($allRecords);
-        system::printTable($table);
+        table::returnTable($table);
     }
 }
 
-class system{
-    /**
-     * @param $table Return the printed table
-     */
-    public static function printTable($table){
-        echo $table;
+class table{
+    public static function returnTable($table) {
+        echo '<html lang="en">';
+        echo '<head>
+                    <!-- Required meta tags -->
+                    <meta charset=\\\'utf - 8\\>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <!-- Bootstrap CSS -->
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                    <title>Jonathan Chang</title>
+                </head>';
+        echo '<table class="table table-striped">' . htmlspecialchars($table). '</tbody> </table>';
+        echo '</html>';
+    }
+
+    public static function returnTableHeader(Array $array = null){
+        echo '<thead>';
+        echo '<tr>';
+        foreach($array as $data){
+            echo '<th>' . htmlspecialchars($data) . '</th>';
+        }
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+    }
+
+    public static function returnTableData(Array $array = null){
+        echo '<tr>';
+        foreach($array as $data){
+            echo '<td>' . htmlspecialchars($data) . '</td>';
+        }
+        echo '</tr>';
     }
 }
 
 class html{
     /**
      * @param $allRecords
-     *          Take the array generated from the CSV file and output it to an HTML table
+     *          Take the array generated from the CSV file and set data to output as HTML
      */
     public static function makeTable($allRecords){
         $count = 0;
         // start table
-        echo "<html lang=\"en\">
-                <head>
-                    <!-- Required meta tags -->
-                    <meta charset=\\'utf - 8\\>
-                    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
-                    <!-- Bootstrap CSS -->
-                    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
-                    <title>Jonathan Chang</title>
-                </head>
-                <table class=\"table table-striped\">
-                <thead>";
-
         foreach($allRecords as $record){
             if ($count == 0){
                 $array = $record -> returnArray();
                 $fields = array_keys($array);
                 //output the header rows
-                self::dataOut($fields, $style = 1);
-
-                echo "</thead>
-                        <tbody>";
+                self::returnTableHeader($fields);
 
                 //output the first data row
                 $values = array_values($array);
-                self::dataOut($values);
+                self::returnTableData($values);
             }
             else{
                 $array = $record -> returnArray();
                 $values = array_values($array);
                 //output the other data rows
-                self::dataOut($values);
+                self::returnTableData($values);
             }
             $count++;
         }
-        echo "</tbody>
-                </table>
-                </html>";
-        return ;
-    }
-
-    /**
-     * @param array|null $array
-     *        Read in either the header row array or the data row array
-     * @param int $style
-     *        Read in either 1 for header row markup or 2 for data row markup
-     */
-    private static function dataOut(Array $array = null, $style = 2){
-        echo '<tr>';
-        foreach($array as $data){
-            if($style ==1){
-                echo '<th>' . htmlspecialchars($data) . '</th>';
-            }
-            else{
-                echo '<td>' . htmlspecialchars($data) . '</td>';
-            }
-        }
-        echo '</tr>';
     }
 }
 
